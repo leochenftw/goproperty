95896a3ef4f1cd87a3bc092d1a07001dae5cfe5a
Leo Chen <leochenftw@gmail.com>
2017-07-19 11:12:45 -1200

    lock menu manager to 2.2.1

--- mainsite/code/Forms/UpdatePasswordForm.php
+++ mainsite/code/Forms/UpdatePasswordForm.php
@@ -11,20 +11,33 @@ class UpdatePasswordForm extends Form {
 		);
 
         $required_fields = new RequiredFields(array('Password'));
-
+        // Debugger::inspect(Session::get_all());
 		parent::__construct($controller, 'UpdatePasswordForm', $fields, $actions, $required_fields);
 		$this->setFormMethod('POST', true)
 			 ->setFormAction(Controller::join_links(BASE_URL, 'member', "UpdatePasswordForm"));
 	}
 
 	public function doUpdate($data, $form) {
+        // Debugger::inspect(Session::get_all());
+        Session::clear('FormInfo');
 		if (!empty($data['SecurityID']) && $data['SecurityID'] == Session::get('SecurityID')) {
-
 			if ($member = Member::currentUser()) {
-				$form->saveInto($member);
-                $member->ChangePassOnNextLogin = false;
-				$member->write();
-                $this->sessionMessage('Your password has been updated.', 'is-success notification');
+
+                $check = $this->PassCheck($member->Email, $data['Password']['_Password']);
+
+                if ($check['status']) {
+    				$form->saveInto($member);
+                    $member->ChangePassOnNextLogin = false;
+    				$member->write();
+                    $this->sessionMessage('Your password has been updated.', 'is-success notification', false);
+                } else {
+                    $messages = $check['messages'];
+                    $refined_message = '';
+                    foreach ($messages as $message) {
+                        $refined_message .= $message . "<br />";
+                    }
+                    $this->sessionMessage(rtrim($refined_message, '<br />'), 'is-danger notification', false);
+                }
 			}
 
 			return Controller::curr()->redirectBack();
@@ -33,4 +46,33 @@ class UpdatePasswordForm extends Form {
 		return Controller::curr()->httpError(400);
 
 	}
+
+    private function PassCheck($user_name, $pass) {
+        $status = true;
+        $message = array();
+        if ($user_name == $pass) {
+            $status = false;
+            $message[] = 'Email and password cannot be the same!';
+        }
+
+        if (strlen($pass) < 6) {
+            $status = false;
+            if (strlen($pass) == 0) {
+                $message[] = "Password can't be empty!";
+            } else {
+                $message[] = "Password too short!";
+            }
+        }
+
+        if (!preg_match("#[0-9]+#", $pass)) {
+            $status = false;
+            $message[] = "Password must include at least one number!";
+        }
+
+        if (!preg_match("#[a-zA-Z]+#", $pass)) {
+            $status = false;
+            $message[] = "Password must include at least one letter!";
+        }
+        return array('status' => $status, 'messages' => $message);
+    }
 }
